bbe06042f69bb495bda60d6c6461a0c93a22e051
gxcuimax <1520771719@qq.com>
2025-08-18 20:35:13 +1200

    add dataset

---
+++ trend.sql
@@ -0,0 +1,8 @@
+-- extract data
+.open trend.db
+.import --csv --schema temp data\cps_00001.csv demograpy
+.import --csv --schema temp data\Ecommerce_data.csv ecommerce
+.import --csv --schema temp data\US_youtube_trending_data.csv youtobe
+
+.header on
+
