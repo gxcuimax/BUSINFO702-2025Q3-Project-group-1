31934c34cf0889a42f293a757aa56e9d305c42e0
gxcuimax <1520771719@qq.com>
2025-08-19 17:42:56 +1200

    ERD

--- trend.sql
+++ trend.sql
@@ -1,4 +1,5 @@
--- extract data
+--sqlite3
+--extract data
 .open trend.db
 .import --csv --schema temp data\cps_00001.csv demograpy
 .import --csv --schema temp data\Ecommerce_data.csv ecommerce
@@ -6,3 +7,4 @@
 
 .header on
 
+SELECT DISTINCT customer_segment FROM ecommerce;
