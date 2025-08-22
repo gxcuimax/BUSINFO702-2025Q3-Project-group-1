b2db47777b886eae5b32907987a963dfbf9a15a0
gxcuimax <1520771719@qq.com>
2025-08-22 22:14:08 +1200

    starschema

--- trend.sql
+++ trend.sql
@@ -1,10 +1,9 @@
 --sqlite3
 --extract data
 .open trend.db
-.import --csv --schema temp data\cps_00001.csv demograpy
+.import --csv --schema temp data\cps_00001.csv demography
 .import --csv --schema temp data\Ecommerce_data.csv ecommerce
 .import --csv --schema temp data\US_youtube_trending_data.csv youtobe
 
-.header on
+.headers on
 
-SELECT DISTINCT customer_segment FROM ecommerce;
